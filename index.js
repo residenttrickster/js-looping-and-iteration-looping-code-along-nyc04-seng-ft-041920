6e8239f4a479e8379764c16acf093f4f5cff46a5
residenttrickster <metkusblackus@gmail.com>
2020-06-20 20:03:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(array, name) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${name} gift!`);
+    }
+    return newArray
+}
+
+function countDown(num) {
+    let i = num;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
+
